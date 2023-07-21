1327a4fed16164081305a6c67bf7595f88d78544
laomao <1303526565@qq.com>
2023-07-22 01:58:52 +0800

    案例_网易新闻接口_其它接口的使用

--- NODE的三天/案例_网易新闻接口/server.js
+++ NODE的三天/案例_网易新闻接口/server.js
@@ -1,15 +1,30 @@
 import http from 'http'
 import axios from 'axios'
 const server = http.createServer()
+
+
+let apiObj = {
+  '/api/ypds': 'http://c.3g.163.com/nc/article/list/T1348649079062/0-20.html ',
+  '/api/pydyuj': 'http://c.3g.163.com/nc/article/local/5bmz6aG25bGx/0-20.html',
+  '/api/mjhw': 'http://c.m.163.com/nc/article/list/T1444270454635/0-20.html',
+  '/api/ggdo': 'http://c.m.163.com/nc/article/list/T1444270454635/20-20.html',
+}
 server.on('request', (req, res) => {
-  if (req.url === '/api/head') {
-    axios({
-      url: 'https://sports.163.com/special/00051C9E/gjb_02.html'
-    }).then(result => {
-      //给请求返回数据
-      res.setHeader('Content-Type', 'application/json; charset=utf-8')
-      res.end(JSON.stringify(result.data))
-    })
+  if (req.url.startsWith('/api')) {
+    if (apiObj[req.url]) {
+      axios({
+        url: apiObj[req.url]
+      }).then(result => {
+        //给请求返回数据
+        res.setHeader('Content-Type', 'application/json; charset=utf-8')
+        res.end(JSON.stringify(result.data))
+      })
+    } else {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+      res.end('api地址不存在请核对,NotFound')
+    }
+
   } else {
     //前端请求其它地址都走这个提示
     res.setHeader('Content-Type', 'text/plain; charset=utf-8')
